00663cb8fc6b99fb0bc002edaac205d772696f1d
Yun-Fan Chen <o07080826mina@gmail.com>
2021-10-24 19:56:09 +0800

    練習5-admin/dashboard

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,12 @@ Route::get('dashboard',function(){
     return'dashboard';
 });
 
+Route::group(['prefix'=>'admin'],function(){
+    Route::get('dashboard',function(){
+        return'admin dashboard';
+    });
+});
+
 /*
 Route::get('/', function () {
     return view('welcome');
